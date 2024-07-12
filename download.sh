660f042a4d5efcfd354d239db439dfc33413671f
Osilvfe <64585305+Osilvfe@users.noreply.github.com>
2024-07-12 21:02:23 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -15,4 +15,4 @@ curl 'https://1400q5-my.sharepoint.com/personal/microadmin_1400q5_onmicrosoft_co
   -H 'service-worker-navigation-preload: {"supportsFeatures":[]}' \
   -H 'upgrade-insecure-requests: 1' \
   -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0'
-  --output super.img
+  -o super.img
